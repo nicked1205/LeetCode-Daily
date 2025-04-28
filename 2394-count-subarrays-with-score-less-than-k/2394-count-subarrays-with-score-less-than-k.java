9334315307380f6c3d58acc4341851434922183a
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-04-28 23:06:26 +1000

    Time: 3 ms (35.02%), Space: 61.6 MB (35.48%) - LeetHub

---
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    long count;
+
+    public Solution() {
+        count = 0;
+    }
+
+    public long countSubarrays(int[] nums, long k) {
+       int i = 0;
+        long sum = 0;
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+            while (i <= j && sum * (j - i + 1) >= k) {
+                sum -= nums[i];
+                i++;
+            }
+            count += j - i + 1;
+        }
+        return count;
+    }
+}
