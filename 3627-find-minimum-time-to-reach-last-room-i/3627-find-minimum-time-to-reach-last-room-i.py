c97a2573c026d307c903277197aac9a0c6336f1d
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-05-08 02:37:13 +1000

    Time: 129 ms (66.76%), Space: 18.8 MB (12.26%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        maxR = len(moveTime)
+        maxC = len(moveTime[0])
+        pq = []
+        dist = [sys.maxsize] * (maxR * maxC)
+        visited = set()
+
+        heapq.heappush(pq, [0, [0, 0]]) 
+        dist[0] = 0
+
+        while pq:
+            curDist, (curX, curY) = heapq.heappop(pq)
+            if (curX, curY) in visited:
+                continue
+            visited.add((curX, curY))
+
+            for dir in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                newX, newY = curX + dir[0], curY + dir[1]
+
+                if not (0 <= newX < maxR and 0 <= newY < maxC):
+                    continue
+
+                if curDist < moveTime[curX + dir[0]][curY + dir[1]]:
+                    newDist = moveTime[curX + dir[0]][curY + dir[1]] + 1
+                else:
+                    newDist = curDist + 1
+                if newDist < dist[newX * maxC + newY]:
+                    dist[newX * maxC + newY] = newDist
+                    heapq.heappush(pq, [newDist, [newX, newY]])
+
+        return dist[maxR * maxC - 1]
