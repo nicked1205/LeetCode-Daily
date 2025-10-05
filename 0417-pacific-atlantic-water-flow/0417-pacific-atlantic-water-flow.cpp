bbf9e4c32a7ee8e353fc45592907eb8d45df17da
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-10-05 20:23:57 +1030

    Time: 9 ms (48.65%), Space: 22.9 MB (66.02%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,48 +1,58 @@
 class Solution {
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int m = heights.size(), n = heights[0].size();
+        vector<vector<bool>> pac(m, vector<bool>(n, false));
+        vector<vector<bool>> atl(m, vector<bool>(n, false));
+        
+        queue<pair<int,int>> pacQ, atlQ;
+        
+        for (int i = 0; i < m; i++) {
+            pacQ.push({i, 0});
+            pac[i][0] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            pacQ.push({0, j});
+            pac[0][j] = true;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            atlQ.push({i, n-1});
+            atl[i][n-1] = true;
+        }
+        for (int j = 0; j < n; j++) {
+            atlQ.push({m-1, j});
+            atl[m-1][j] = true;
+        }
+        
+        bfs(heights, pacQ, pac);
+        bfs(heights, atlQ, atl);
+        
         vector<vector<int>> res;
-        set<vector<int>> atl;
-        set<vector<int>> pac;
-
-        int m = heights.size();
-        int n = heights[0].size();
-
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    flowInward(heights, pac, i, j);
-                }
-                if (i == m - 1 || j == n - 1) {
-                    flowInward(heights, atl, i, j);
-                }
+                if (pac[i][j] && atl[i][j])
+                    res.push_back({i, j});
             }
         }
-
-        for (auto coords : pac) {
-            if (atl.count(coords)) res.push_back(coords);
-        }
-
         return res;
     }
 
-    void flowInward(vector<vector<int>>& heights, set<vector<int>>& sea, int i, int j) {
-        int m = heights.size();
-        int n = heights[0].size();
-
-        cout << "Exploring " << i << ", " << j << endl;
-        sea.insert({i, j});
-        vector<vector<int>> directions = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        for (auto dir : directions) {
-            int ni = i + dir[0];
-            int nj = j + dir[1];
-
-            if (ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
-
-            if (sea.count({ni, nj})) continue;
-
-            if (heights[ni][nj] >= heights[i][j]) {
-                flowInward(heights, sea, ni, nj);
+private:
+    void bfs(vector<vector<int>>& heights, queue<pair<int,int>>& q, vector<vector<bool>>& vis) {
+        int m = heights.size(), n = heights[0].size();
+        vector<pair<int,int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        while (!q.empty()) {
+            auto [i, j] = q.front(); q.pop();
+            for (auto [di, dj] : dirs) {
+                int ni = i + di, nj = j + dj;
+                if (ni < 0 || nj < 0 || ni >= m || nj >= n) continue;
+                if (vis[ni][nj]) continue;
+                if (heights[ni][nj] >= heights[i][j]) {
+                    vis[ni][nj] = true;
+                    q.push({ni, nj});
+                }
             }
         }
     }
