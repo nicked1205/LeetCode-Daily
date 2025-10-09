443cd0863c256474614129e1719c54944ed74928
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-10-10 03:05:34 +1030

    Time: 904 ms (5.01%), Space: 40.9 MB (19.44%) - LeetHub

--- 1428-jump-game-iii/1428-jump-game-iii.cpp
+++ 1428-jump-game-iii/1428-jump-game-iii.cpp
@@ -8,15 +8,14 @@ public:
 
     bool explore(int i, vector<int>& arr, vector<int>& visited, int& n) {
         bool res = false;
-        cout << "Exploring " << i << ": " << arr[i] << endl;
         if (arr[i] == 0) return true;
         if (i + arr[i] < n && find(visited.begin(), visited.end(), i + arr[i]) == visited.end()) {
             visited.push_back(i + arr[i]);
-            if (explore(i + arr[i], arr, visited, n)) res = true;
+            if (explore(i + arr[i], arr, visited, n)) return true;
         }
         if (i - arr[i] >= 0 && find(visited.begin(), visited.end(), i - arr[i]) == visited.end()) {
             visited.push_back(i - arr[i]);
-            if(explore(i - arr[i], arr, visited, n)) res = true;
+            if(explore(i - arr[i], arr, visited, n)) return true;
         }
         cout << res << endl;
 
