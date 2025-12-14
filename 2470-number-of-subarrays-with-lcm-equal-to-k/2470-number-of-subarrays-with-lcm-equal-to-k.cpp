202afae0a416879075a253718d8a2e7fd1008285
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-12-15 03:37:36 +1030

    Time: 8 ms (72.57%), Space: 13.1 MB (99.43%) - LeetHub

--- 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
+++ 2470-number-of-subarrays-with-lcm-equal-to-k/2470-number-of-subarrays-with-lcm-equal-to-k.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        return b == 0 ? a : gcd(b, a % b);
-    }
-
-    int lcm(int a, int b) {
-        return a / gcd(a, b) * b;
-    }
-
     int subarrayLCM(vector<int>& nums, int k) {
-        int n = nums.size();
-        int res = 0;
-
-        for (int i = 0; i < n; i++) {
-            int curLCM = 1;
-            for (int j = i; j < n; j++) {
-                curLCM = lcm(curLCM, nums[j]);
-
-                if (curLCM == k) res++;
-                if (curLCM > k || k % curLCM != 0) break;
+        int res = 0, n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            long long curr = nums[i];
+            for (int j = i; j < n; ++j) {
+                curr = std::lcm((long long)curr, (long long)nums[j]);
+                if (curr == k) ++res;
+                else if (curr > k || k % curr != 0) break;
             }
         }
-
         return res;
     }
 };
