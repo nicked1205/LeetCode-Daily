b3661116196c1d1feccc97bcee2c8bde837cca9f
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-12-17 00:09:22 +1030

    Time: 0 ms (100%), Space: 14.6 MB (87.69%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        unordered_map<char, int> count;
-        unordered_map<char, int> firstPos;
-
-        for (int i = 0; i < s.size(); i++) {
-            count[s[i]]++;
-            if (!firstPos.count(s[i])) {
-                firstPos[s[i]] = i;
+        vector<int> freq(26,0);
+        int min_idx = INT_MAX;
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(freq[s[i]-'a']==1){
+                return i;
             }
         }
-
-        int ans = INT_MAX;
-        for (auto& [c, cnt] : count) {
-            if (cnt == 1) {
-                ans = min(ans, firstPos[c]);
-            }
-        }
-
-        return ans == INT_MAX ? -1 : ans;
+        return -1;
     }
 };
