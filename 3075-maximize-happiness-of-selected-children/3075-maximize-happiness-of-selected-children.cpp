857d7ec1b98437bca7830c106dd6e940a05779ce
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-12-25 16:35:28 +1030

    Time: 153 ms (69.5%), Space: 107.6 MB (100%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -3,12 +3,10 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         long long res = 0;
         sort(happiness.begin(), happiness.end(), greater<int>());
-        int dec = 0;
         for (int i = 0; i < k; i++) {
-            int val = happiness[i] - dec;
+            int val = happiness[i] - i;
             if (val <= 0) break;
             res += val;
-            dec++;
         }
 
         return res;
