c4459e7dee2054682b05be0855f84f257e9665f0
Cat Tuong Anh Nguyen <141990600+nicked1205@users.noreply.github.com>
2025-12-27 21:53:00 +1030

    Time: 0 ms (100%), Space: 206.2 MB (72.19%) - LeetHub

--- 3523-make-array-non-decreasing/3523-make-array-non-decreasing.cpp
+++ 3523-make-array-non-decreasing/3523-make-array-non-decreasing.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     int maximumPossibleSize(vector<int>& nums) {
-        vector<int> st;
+        int res = 0;
+        int pre = 0;
         for (int n : nums) {
-            if (st.size() == 0 || (st.size() > 0 && n >= st.back())) st.push_back(n);
+            if (n >= pre) {
+                pre = n;
+                res++;
+            }
         }
 
-        return st.size();
+        return res;
     }
 };
